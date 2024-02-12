2048b33adb99db222983e83748165ba64fe27797
Evandro Junior <ecsb.junior@gmail.com>
2024-02-12 09:54:55 -0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  trailingComma: 'all',
+  singleAttributePerLine: true,
+  endOfLine: 'lf',
+};
